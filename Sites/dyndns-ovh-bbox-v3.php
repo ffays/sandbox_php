9d217322223f9f967099af0ee409c8f236be239d
Frédéric Fays <frederic@fays.eu>
2018-09-16 15:11:22 +0200

    OVH dyndns with apache RewriteRule
    	new file:   .htaccess
    	modified:   dyndns-ovh-bbox-v3.php

--- Sites/dyndns-ovh-bbox-v3.php
+++ Sites/dyndns-ovh-bbox-v3.php
@@ -1,11 +1,11 @@
 <?php
 header("Content-Type: text/plain");
-if(!isset($_SERVER['PHP_AUTH_USER'])) {
+// foreach($_SERVER as $key => $value) { header("X-Debug-" . $key . ": "  . $value); }
+if(!isset($_SERVER['HTTP_AUTHORIZATION'])) {
 	header('www-authenticate: Basic realm="What is your nic handle and password ??"');
 	return;
 }
-$authorization = base64_encode($_SERVER['PHP_AUTH_USER'] . ':' . $_SERVER['PHP_AUTH_PW']);
-$options = array('http' => array('method'  => 'GET', 'header' => 'Authorization: Basic ' . $authorization));
+$options = array('http' => array('method'  => 'GET', 'header' => 'Authorization: ' . $_SERVER['HTTP_AUTHORIZATION']));
 $context  = stream_context_create($options);
 $url = 'https://www.ovh.com/nic/update?' . $_SERVER['QUERY_STRING'] . "&system=dyndns";
 $response = file_get_contents($url, false, $context);
