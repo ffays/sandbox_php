c6484c1a2b328b95373d0cdd7b04d7970f5b4eb9
Frédéric Fays <frederic@mach-17.lan>
2018-09-16 14:48:44 +0200

    Working wihtout apache_request_headers()

--- Sites/dyndns-ovh-bbox-v3.php
+++ Sites/dyndns-ovh-bbox-v3.php
@@ -1,11 +1,11 @@
 <?php
 header("Content-Type: text/plain");
-$header = apache_request_headers();
-if(!isset($header['Authorization'])) {
+if(!isset($_SERVER['PHP_AUTH_USER'])) {
 	header('www-authenticate: Basic realm="What is your nic handle and password ??"');
 	return;
 }
-$options = array('http' => array('method'  => 'GET', 'header' => 'Authorization: ' . $header['Authorization']));
+$authorization = base64_encode($_SERVER['PHP_AUTH_USER'] . ':' . $_SERVER['PHP_AUTH_PW']);
+$options = array('http' => array('method'  => 'GET', 'header' => 'Authorization: Basic ' . $authorization));
 $context  = stream_context_create($options);
 $url = 'https://www.ovh.com/nic/update?' . $_SERVER['QUERY_STRING'] . "&system=dyndns";
 $response = file_get_contents($url, false, $context);
